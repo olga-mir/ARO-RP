e6c495cc69f6b041f74f6abe549eedbe53b9cb9e
Olga Mirensky <olga.mirensky@redhat.com>
2020-06-24 12:14:11 +1000

    successful install of argocd

--- test/e2e/stateful_app.go
+++ test/e2e/stateful_app.go
@@ -1,24 +1,21 @@
 package e2e
 
 import (
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-	"github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"time"
 
 	"github.com/Azure/ARO-RP/test/util/project"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	v1 "github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1"
+	"github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 )
 
-const (
-	packageName   = "busybox"
-	channelName   = "alpha"
-	subName       = "test-subscription"
-	secretName    = "mysecret"
-	configmapName = "special-config"
-	testNamespace = "test-e2e"
-	sourceName    = "argocd-catalog"
-	imageName     = "quay.io/jmckind/argocd-operator-registry@sha256:45f9b0ad3722cd45125347e4e9d149728200221341583a998158d1b5b4761756"
-)
+const pollInterval = 1 * time.Second
+const pollDuration = 5 * time.Minute
+const testNamespace = "test-e2e"
 
 var _ = BeforeEach(func() {
 	err := project.CreateProject(clients.Project, testNamespace)
@@ -33,34 +30,30 @@ var _ = AfterEach(func() {
 var _ = Describe("Stateful app", func() {
 	FSpecify("should create and validate test apps", func() {
 
-		// create catalog source
-		source := &v1alpha1.CatalogSource{
-			TypeMeta: metav1.TypeMeta{
-				Kind:       v1alpha1.CatalogSourceKind,
-				APIVersion: v1alpha1.CatalogSourceCRDAPIVersion,
-			},
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      sourceName,
-				Namespace: testNamespace,
-				Labels:    map[string]string{"olm.catalogSource": sourceName},
-			},
-			Spec: v1alpha1.CatalogSourceSpec{
-				SourceType: v1alpha1.SourceTypeGrpc,
-				Image:      imageName,
-			},
-		}
+		const (
+			channelName       = "alpha"
+			subscriptionName  = "argocd-operator"
+			operatorName      = "argocd-operator"
+			catalogSourceName = "argocd-catalog"
+			image             = "quay.io/jmckind/argocd-operator-registry@sha256:45f9b0ad3722cd45125347e4e9d149728200221341583a998158d1b5b4761756"
+		)
 
-		source, err := clients.OLMClient.OperatorsV1alpha1().CatalogSources(source.GetNamespace()).Create(source)
-		Expect(err).ToNot(HaveOccurred(), "could not create catalog source")
+		catalogSource, err := createCatalogSource(catalogSourceName, testNamespace, image)
+		Expect(err).ToNot(HaveOccurred(), "failed to create catalog source")
+		time.Sleep(30 * time.Second)
 
-		// Create a Subscription for package
-		// _ = createSubscriptionForCatalog(operatorClient, source.GetNamespace(), subName, source.GetName(), packageName, channelName, "", v1alpha1.ApprovalAutomatic)
+		_, err = createOperatorGroup(catalogSource, operatorName)
+		Expect(err).ToNot(HaveOccurred(), "Failed to create operator group")
+		time.Sleep(30 * time.Second)
+
+		sub, err := createSubscriptionForCatalog(catalogSource, subscriptionName, channelName)
+		Expect(err).ToNot(HaveOccurred(), "Failed to create subscription")
 
 		// Wait for the Subscription to succeed
-		// Eventually(func() error {
-		// 	_, err = fetchSubscription(operatorClient, testNamespace, subName, subscriptionStateAtLatestChecker)
-		// 	return err
-		// }).Should(BeNil())
+		Eventually(func() error {
+			_, err = fetchSubscription(testNamespace, sub.GetName())
+			return err
+		}).Should(BeNil())
 
 		// confirm extra bundle objects (secret and configmap) are installed
 
@@ -69,17 +62,82 @@ var _ = Describe("Stateful app", func() {
 		// 	return err
 		// }).Should(Succeed(), "expected no error getting configmap object associated with CSV")
 
-		// ctx := context.Background()
-		// By("creating test app")
-		// namespace, errs := sanity.Checker.CreateTestApp(ctx)
-		// Expect(errs).To(BeEmpty())
-		// defer func() {
-		// 	By("deleting test app")
-		// 	_ = sanity.Checker.DeleteTestApp(ctx, namespace)
-		// }()
-
-		// By("validating test app")
-		// errs = sanity.Checker.ValidateTestApp(ctx, namespace)
-		// Expect(errs).To(BeEmpty())
 	})
 })
+
+func createCatalogSource(catalogSourceName, namespace, image string) (*v1alpha1.CatalogSource, error) {
+	catalogSource := &v1alpha1.CatalogSource{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       v1alpha1.CatalogSourceKind,
+			APIVersion: v1alpha1.CatalogSourceCRDAPIVersion,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      catalogSourceName,
+			Namespace: namespace,
+			Labels:    map[string]string{"olm.catalogSource": catalogSourceName},
+		},
+		Spec: v1alpha1.CatalogSourceSpec{
+			SourceType:  v1alpha1.SourceTypeGrpc,
+			Image:       image,
+			DisplayName: "Argo CD Operators",
+			Publisher:   "Argo CD Community",
+		},
+	}
+	return clients.OLMClient.OperatorsV1alpha1().CatalogSources(namespace).Create(catalogSource)
+}
+
+func createOperatorGroup(catalogSource *v1alpha1.CatalogSource, operatorName string) (*v1.OperatorGroup, error) {
+	ns := catalogSource.GetNamespace()
+	operatorGroup := &v1.OperatorGroup{
+		TypeMeta: metav1.TypeMeta{
+			Kind: v1.OperatorGroupKind,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: operatorName,
+		},
+		Spec: v1.OperatorGroupSpec{
+			TargetNamespaces: []string{
+				ns,
+			},
+		},
+	}
+
+	return clients.OLMClient.OperatorsV1().OperatorGroups(ns).Create(operatorGroup)
+}
+
+func createSubscriptionForCatalog(catalogSource *v1alpha1.CatalogSource, name, channel string) (*v1alpha1.Subscription, error) {
+	ns := catalogSource.GetNamespace()
+	subscription := &v1alpha1.Subscription{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       v1alpha1.SubscriptionKind,
+			APIVersion: v1alpha1.SubscriptionCRDAPIVersion,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: ns,
+			Name:      name,
+		},
+		Spec: &v1alpha1.SubscriptionSpec{
+			CatalogSource:          catalogSource.GetName(),
+			CatalogSourceNamespace: ns,
+			Package:                "argocd-operator",
+			Channel:                channel,
+			InstallPlanApproval:    v1alpha1.ApprovalAutomatic,
+		},
+	}
+
+	return clients.OLMClient.OperatorsV1alpha1().Subscriptions(ns).Create(subscription)
+}
+
+func fetchSubscription(namespace, name string) (*v1alpha1.Subscription, error) {
+	var fetchedSubscription *v1alpha1.Subscription
+	var err error
+
+	err = wait.Poll(pollInterval, pollDuration, func() (bool, error) {
+		fetchedSubscription, err = clients.OLMClient.OperatorsV1alpha1().Subscriptions(namespace).Get(name, metav1.GetOptions{})
+		if err != nil || fetchedSubscription == nil {
+			return false, err
+		}
+		return true, nil
+	})
+	return fetchedSubscription, err
+}
